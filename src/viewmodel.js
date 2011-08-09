99361f8a5e06cd06876295362c2620a033607f8a
Michal Kuklis <michal.kuklis@gmail.com>
2011-08-08 23:51:33 -0400

    some more work on viewmodel

--- src/viewmodel.js
+++ src/viewmodel.js
@@ -27,21 +27,39 @@
     }
   };
 
+  // TODO: should ViewModel be a mixin
+  // which extends regular backbone view
+  // if `observable` is present?
   R.ViewModel = Backbone.View.extend({
-    initialize: function () {},
+    initialize: function (options) {
+      this.attr = options.attr;
+    },
 
     // updates view with model's value
     _updateView: function () {
+      this.el.val(this.model.get(this.attr));
     },
 
     // updates model with current value from view
     _updateModel: function () {
+      var keyValue = {};
+      keyValue[this.attr] = this._getValue();
+      this.model.set(keyValue);
+    },
+
+    // returns view's value
+    // this is a default implementation
+    // other views should override this method
+    _getValue: function () {
+      return this.el.val();
     }
   });
 
-  R.ViewModel.factory = function (el) {
+  // Factory which produces new ViewModel based
+  // on given element type
+  R.ViewModel.factory = function (el, options) {
     var constr = cel.getType();
-    return new R[constr];
+    return new R[constr](options);
   }
 
 })(Ribs);
